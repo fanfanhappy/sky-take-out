a0244ceb97de11ab4371e7d913f66cc4594d5746
Skerio <3442669768@qq.com>
2024-05-15 11:08:51 +0800

    店铺营业状态设置

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,44 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @projectName: sky-take-out
+ * @package: com.sky.controller.admin
+ * @className: ShopController
+ * @author: 姬紫衣
+ * @description: TODO
+ * @date: 2024/5/15 10:43
+ * @version: 1.0
+ */
+
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+@Api(tags = "店铺相关接口")
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    /**
+     * 获取店铺营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺营业状态")
+    public Result<Integer> getShopStatus()
+    {
+        Integer status =(Integer) redisTemplate.opsForValue().get("SHOP_STATUS");
+        log.info("获取店铺营业状态：{}" , status);
+        return Result.success(status);
+    }
+}
